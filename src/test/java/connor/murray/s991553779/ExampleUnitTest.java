f362204927c02d7977a55e8058183c3381da3ba0
murrconn <murrconn@sheridancollege.ca>
2021-03-28 15:43:55 -0400

    initial commit

---
+++ src/test/java/connor/murray/s991553779/ExampleUnitTest.java
@@ -0,0 +1,22 @@
+/*
+Connor Murray
+Student ID: 991553779
+Section #34780
+ */
+package connor.murray.s991553779;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
