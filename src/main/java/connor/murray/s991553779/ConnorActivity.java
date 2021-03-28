76f76a916957994e8f8be512ad24f75034060339
murrconn <murrconn@sheridancollege.ca>
2021-03-28 19:28:40 -0400

    third commit, fixed fragment code, first and second fragment work via bottom nagivation, third still work in progress.

--- src/main/java/connor/murray/s991553779/ConnorActivity.java
+++ src/main/java/connor/murray/s991553779/ConnorActivity.java
@@ -6,14 +6,17 @@ Section #34780
 package connor.murray.s991553779;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class ConnorActivity extends AppCompatActivity {
 
@@ -25,12 +28,13 @@ public class ConnorActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+               R.id.connorFragment, R.id.murrayFragment, R.id.S991553779Fragment)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.connorNav_host_fragment);
-        //NavController navController = Navigation.findNavController(this, R.id.connorNav_host_fragment);
+
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
+
 
 }
--- src/main/java/connor/murray/s991553779/ConnorFragment.java
+++ src/main/java/connor/murray/s991553779/ConnorFragment.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,17 +20,12 @@ import android.view.ViewGroup;
 
 public class ConnorFragment extends Fragment {
 
-
-    public static ConnorFragment newInstance() {
-        return new ConnorFragment();
-    }
-
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.connor_fragment, container, false);
     }
-
-
-
 }
