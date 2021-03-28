da6e4911971c5f7f3fa733e1d4f7d357cf2f80dc
murrconn <murrconn@sheridancollege.ca>
2021-03-28 16:39:34 -0400

    Second commit, changed fragment files, and added name student section, changed colors, added strings.xml, created gitignore, textview, color change

--- src/main/java/connor/murray/s991553779/ConnorActivity.java
+++ src/main/java/connor/murray/s991553779/ConnorActivity.java
@@ -20,7 +20,7 @@ public class ConnorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connor);
         BottomNavigationView navView = findViewById(R.id.connorNav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -28,6 +28,7 @@ public class ConnorActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.connorNav_host_fragment);
+        //NavController navController = Navigation.findNavController(this, R.id.connorNav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
