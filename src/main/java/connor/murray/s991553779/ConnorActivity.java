f110d6fbf91203cc83ec8f48333658a15c10a3eb
murrconn <murrconn@sheridancollege.ca>
2021-03-28 20:33:13 -0400

    fifith commit, new launcher icon added, fixed ID of nav host to include first name.

--- src/main/java/connor/murray/s991553779/ConnorActivity.java
+++ src/main/java/connor/murray/s991553779/ConnorActivity.java
@@ -6,17 +6,14 @@ Section #34780
 package connor.murray.s991553779;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.navigation.fragment.NavHostFragment;
 
 public class ConnorActivity extends AppCompatActivity {
 
@@ -31,7 +28,7 @@ public class ConnorActivity extends AppCompatActivity {
                R.id.connorFragment, R.id.murrayFragment, R.id.S991553779Fragment)
                 .build();
 
-        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this,R.id.connorNav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
